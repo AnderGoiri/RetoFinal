aca4ae4fa29c049074d63adbc66a152d88cfc12a
sunder-012 <1dami@TartangaLH.eus>
2023-04-25 13:21:52 +0200

    Feat: Exceptions added

--- src/logicTier/ProductMemberControllable.java
+++ src/logicTier/ProductMemberControllable.java
@@ -2,24 +2,29 @@ package logicTier;
 
 import java.util.Set;
 
+import exceptions.BrandNotFoundException;
+import exceptions.ModelNotFoundException;
+import exceptions.NameNotFoundException;
+import exceptions.ProductNotFoundException;
+import exceptions.TypeNotFoundException;
 import model.Product;
 
 public interface ProductMemberControllable {
-	public Set<Product> searchProduct(String search);
+	public Set<Product> searchProduct(String search) throws ProductNotFoundException;
 	
-	public Set<Product> searchProductByName(String name);
+	public Set<Product> searchProductByName(String name) throws NameNotFoundException;
 	
-	public Set<Product> searchProductByBrand(String brand);
+	public Set<Product> searchProductByBrand(String brand) throws BrandNotFoundException;
 	
-	public Set<Product> searchProductByModel(String model);
+	public Set<Product> searchProductByModel(String model) throws ModelNotFoundException;
 	
-	public Set<Product> searchProductByType(String type);
+	public Set<Product> searchProductByType(String type) throws TypeNotFoundException;
 	
-	public Product purchaseProduct(Product p);
+	public Product purchaseProduct(Product p) throws ProductNotFoundException;
 	
-	public Product checkProduct(Product p);
+	public Product checkProduct(Product p) throws ProductNotFoundException;
 	
-	public Set<Product> checkPurchaseRecord(Product p);
+	public Set<Product> checkPurchaseRecord(Product p) throws ProductNotFoundException;
 	
 	
 }
