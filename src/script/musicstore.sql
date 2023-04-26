c67e7d60897c2613ed83e02ed823a16283d06e11
sunder-012 <1dami@TartangaLH.eus>
2023-04-26 11:01:10 +0200

    Fix: Added inserts for Admin, TODO SELECT

--- src/script/musicstore.sql
+++ src/script/musicstore.sql
@@ -4,7 +4,7 @@ use musicstore;
 
 /*TABLE CREATION*/
 create table if not exists user(
-	idUser integer not null,
+	idUser integer not null unique,
     username varchar(25),
     password varchar(25),
     name varchar(25),
@@ -15,25 +15,25 @@ create table if not exists user(
 );
 
 create table if not exists manager(
-	idUser integer,
+	idUser integer not null unique,
 	isTechnician tinyint,
-    isAdmin tinyint,
+    isAdmin tinyint not null,
     idSupervisor integer,
-    statusManager enum('Pending', 'Approved', 'Rejected'),
-    foreign key (idUser) references user (idUser),
+    statusManager enum('Pending', 'Approved', 'Rejected') not null,
+    foreign key (idUser) references user (idUser) on delete cascade,
     primary key (idUser)
 );
 
 create table if not exists member(
-	idUser integer not null,
+	idUser integer not null unique,
 	address varchar(100),
     creditCard varchar(16),
-    foreign key (idUser) references user (idUser),
+    foreign key (idUser) references user (idUser) on delete cascade,
     primary key (idUser)
 );
 
 create table if not exists repair(
-	idRepair integer not null,
+	idRepair integer not null unique,
     idUser integer,
     repairCost integer,
     startDate date,
@@ -45,7 +45,7 @@ create table if not exists repair(
 );
 
 create table if not exists product(
-	idProduct integer not null,
+	idProduct integer not null unique,
     brand varchar(20),
     model varchar(20),
     unitPrice integer,
@@ -59,7 +59,7 @@ create table if not exists product(
 );
 
 create table if not exists instrument(
-	idProduct integer not null,
+	idProduct integer not null unique,
 	classInstrument enum('Wind', 'String', 'Percussion'),
     typeInstrument enum('Acoustic', 'Electronic'),
 	foreign key (idProduct) references product (idProduct),
@@ -67,7 +67,7 @@ create table if not exists instrument(
 );
 
 create table if not exists component(
-	idProduct integer not null,
+	idProduct integer not null unique,
 	classComponent enum('Chasis', 'Circuit'),
     typeComponent enum('Architecture', 'Tuning', 'Connection'),
     foreign key (idProduct) references product (idProduct),
@@ -83,7 +83,7 @@ create table if not exists accessory(
 );
 
 create table if not exists purchase(
-	idPurchase integer not null,
+	idPurchase integer not null unique,
     idUser integer,
     idProduct integer,
 	datePurchase date,
@@ -97,7 +97,7 @@ create table if not exists purchase(
 );
 
 create table if not exists management(
-	idManagement integer not null,
+	idManagement integer not null unique,
     idUser integer,
     idProduct integer,
     dateManagement date,
@@ -108,17 +108,17 @@ create table if not exists management(
 	primary key (idManagement)
 );
 
-/*USERS*/
-/*CREATE USER 'ander'@'localhost' IDENTIFIED BY 'abcd*1234';
-GRANT ALL PRIVILEGES ON musicstore.* TO 'ander'@'localhost' WITH GRANT OPTION;
-CREATE USER 'ander'@'192.168.20.255' IDENTIFIED BY 'abcd*1234';
-GRANT ALL PRIVILEGES ON musicstore.* TO 'ander'@'192.168.20.255' WITH GRANT OPTION;
+/*ADMIN USER*/
+CREATE USER 'admin'@'localhost' IDENTIFIED BY 'abcd*1234';
+GRANT ALL PRIVILEGES ON musicstore.* TO 'admin'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
-select user from mysql.user;
-*/
-
 
 insert into user values(
 '0', 'Admin', 'abcd*1234',' ',' ','2023-04-26',' '
 );
+insert into manager values(
+' ', '1', '1', null , 'Approved'
+);
+
+select u.*,  from user u join manager m on u.idUser = m.idUser;
 
