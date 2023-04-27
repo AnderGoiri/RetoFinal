665e35e758f8e680da1e4d9823ae19ffd577d7f8
ysasgon <fdeysasigonzalez@gmail.com>
2023-04-27 10:38:14 +0200

    UPDATE New Shop Windows developed.

--- src/storeMenu/StoreMenu.java
+++ src/storeMenu/StoreMenu.java
@@ -1,9 +1,6 @@
 package storeMenu;
 
-import java.awt.BorderLayout;
-import java.awt.FlowLayout;
 
-import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
@@ -11,7 +8,12 @@ import java.awt.CardLayout;
 
 public class StoreMenu extends JDialog {
 
-	private final JPanel contentPanel = new JPanel();
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPanel;
+	private JPanel menuPanel;
+	
+	private ManagerMenu managerPanel;
+	private CardLayout cardLayout;
 
 	/**
 	 * Launch the application.
@@ -31,10 +33,27 @@ public class StoreMenu extends JDialog {
 	 */
 	public StoreMenu() {
 		setBounds(100, 100, 1900, 1000);
-		getContentPane().setLayout(new BorderLayout());
+		contentPanel = new JPanel();
+		contentPanel.setBounds(0, 0, 1900, 1000);
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
-		getContentPane().add(contentPanel, BorderLayout.CENTER);
-		contentPanel.setLayout(new CardLayout(0, 0));
+		setContentPane(contentPanel);
+		contentPanel.setLayout(null);
+		
+		menuPanel=new JPanel();
+		menuPanel.setBounds(10,0,1860,950);
+		cardLayout = new CardLayout();
+		menuPanel.setLayout(cardLayout);
+		contentPanel.add(menuPanel);
+		
+		managerPanel = new ManagerMenu();
+		menuPanel.add("Manager", managerPanel);
+		
+		showMenu();
+	}
+
+	private void showMenu() {
+		// TODO Auto-generated method stub
+		cardLayout.show(menuPanel, "Manager");
 	}
 
 }
