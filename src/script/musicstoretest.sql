95265ef0d594847d6cd121ab06bd4e4083b32d30
sunder-012 <1dami@TartangaLH.eus>
2023-04-28 08:35:23 +0200

    Fix: Trying procedure in script

--- src/script/musicstoretest.sql
+++ src/script/musicstoretest.sql
@@ -118,7 +118,28 @@ GRANT ALL PRIVILEGES ON musicstoretest.* TO 'ander'@'192.168.20.255' WITH GRANT 
 FLUSH PRIVILEGES;
 select user from mysql.user;
 */
-
+DELIMITER //
+CREATE DEFINER=`root`@`localhost` PROCEDURE `insert_new_member`(
+    IN p_username VARCHAR(25),
+    IN p_name VARCHAR(25),
+    IN p_surname VARCHAR(25),
+    IN p_password VARCHAR(25),
+    IN p_mail VARCHAR(25),
+    IN p_dateRegister DATE,
+    IN p_address VARCHAR(100),
+    IN p_creditCard VARCHAR(16)
+)
+BEGIN
+    -- Insert the new user into the user table
+    INSERT INTO user (username, name, surname, password, mail, dateRegister)
+    VALUES (p_username, p_name, p_surname, p_password, p_mail, p_dateRegister);
+    -- Get the ID of the new user
+    SET @new_user_id = LAST_INSERT_ID();
+    -- Insert the new member into the member table
+    INSERT INTO member (idUser, address, creditCard)
+    VALUES (@new_user_id, p_address, p_creditCard);
+END
+//
 insert into user values(
 'Admin', 'abcd*1234',' ',' ','2023-04-26',' '
 );
