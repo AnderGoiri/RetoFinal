87f0108be492538358e7e587e0bda8eb044aa23d
sunder-012 <1dami@TartangaLH.eus>
2023-04-28 09:03:15 +0200

    In progress: Began admin procedure

--- src/script/musicstoretest.sql
+++ src/script/musicstoretest.sql
@@ -107,9 +107,7 @@ create table if not exists management(
 	primary key (idManagement)
 );
 
-CALL insert_new_member('Prueba4', 'Test', 'User', 'test123', 'testuser@example.com', '1990-01-01', '123 Main St', '1234567890123456');
-/*USERS*/
-
+call insert_new_manager(); 
 /*
 CREATE USER 'root'@'192.168.20.255' IDENTIFIED BY 'abcd*1234';
 GRANT ALL PRIVILEGES ON musicstoretest.* TO 'root'@'192.168.20.255' WITH GRANT OPTION;
@@ -118,32 +116,7 @@ GRANT ALL PRIVILEGES ON musicstoretest.* TO 'ander'@'192.168.20.255' WITH GRANT 
 FLUSH PRIVILEGES;
 select user from mysql.user;
 */
-DELIMITER //
-CREATE DEFINER=`root`@`localhost` PROCEDURE `insert_new_member`(
-    IN p_username VARCHAR(25),
-    IN p_name VARCHAR(25),
-    IN p_surname VARCHAR(25),
-    IN p_password VARCHAR(25),
-    IN p_mail VARCHAR(25),
-    IN p_dateRegister DATE,
-    IN p_address VARCHAR(100),
-    IN p_creditCard VARCHAR(16)
-)
-BEGIN
-    -- Insert the new user into the user table
-    INSERT INTO user (username, name, surname, password, mail, dateRegister)
-    VALUES (p_username, p_name, p_surname, p_password, p_mail, p_dateRegister);
-    -- Get the ID of the new user
-    SET @new_user_id = LAST_INSERT_ID();
-    -- Insert the new member into the member table
-    INSERT INTO member (idUser, address, creditCard)
-    VALUES (@new_user_id, p_address, p_creditCard);
-END
-//
-insert into user values(
-'Admin', 'abcd*1234',' ',' ','2023-04-26',' '
-);
+
 
 select * from user u join member m on u.idUser = m.idUser;
-select * from user;
 
